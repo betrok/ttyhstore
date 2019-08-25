8023676e4d910525ac907e33f448de4873ffc025
betrok <betrok@ya.ru>
2019-08-25 15:41:35 +0300

    fix missing overwrited libs in data.json

--- main.go
+++ main.go
@@ -503,6 +503,7 @@ func checkLib(dl *LibDownload, index FIndex) error {
 				if verbose {
 					log.Printf("Lib \"%s\" already checked\n", dl.Path)
 				}
+				index[dl.Path] = dl.ToFInfo()
 				return nil
 			}
 
